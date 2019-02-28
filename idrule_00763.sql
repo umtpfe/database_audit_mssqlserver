eb1097ff352f75814559c1a244e91ef2c661926b
UMTPFE <47662521+umtpfe@users.noreply.github.com>
2019-02-28 11:18:17 +0100

    Rajout d'une requête convertie

---
+++ idrule_00763.sql
@@ -0,0 +1,18 @@
+/* statistique sur les indexes */
+
+DBCC SHOW_STATISTICS ( indexed_name , target )
+[ WITH [ NO_INFOMSGS ] < option > [ , n ] ]  
+< option > :: =  
+    STAT_HEADER | DENSITY_VECTOR | HISTOGRAM | STATS_STREAM
+DBCC SHOW_STATISTICS ( table_name , target )   
+    [ WITH {STAT_HEADER | DENSITY_VECTOR | HISTOGRAM } [ ,...n ] ]  
+[;]  
+
+
+/* fiche d'identité
+role = DBA
+categorie = CON
+type = SQL
+basetype  = sqlserver
+url = https://docs.microsoft.com/en-us/sql/t-sql/database-console-commands/dbcc-show-statistics-transact-sql?view=sql-server-2017
+etat = 0 */

1d794aa9a53d678b628776067bd18daf38275fb9
UMTPFE <47662521+umtpfe@users.noreply.github.com>
2019-04-01 18:53:59 +0200

    modele logique version temp

--- idrule_00009.sql
+++ idrule_00009.sql
@@ -8,27 +8,42 @@ DECLARE @attribut nvarchar(MAX)
 DECLARE @prec nvarchar(MAX)
 DECLARE @chaine nvarchar(MAX)
 DECLARE @var nvarchar(5)
+DECLARE @var2 nvarchar(10)
 PRINT N'Regle: Les tables utilisateurs:'
 OPEN dync
 FETCH NEXT FROM dync INTO @db, @schem, @taable, @attribut
 SET @chaine = @db + '.' + @schem + '.' + @taable + '('
-	SELECT @var = is_nullable FROM INFORMATION_SCHEMA.COLUMNS 
- 		 		WHERE column_name = @attribut AND TABLE_NAME = @taable 
- 	IF @var = 'YES'
-		SET @chaine = @chaine +'?'
-	SELECT @var = ao.type FROM sys.all_objects ao 
- 		INNER JOIN INFORMATION_SCHEMA.CONSTRAINT_COLUMN_USAGE ic 
- 		ON ic.CONSTRAINT_NAME = ao.name
- 		WHERE ic.column_name = @attribut AND ic.TABLE_NAME = @taable
- 	IF @var = 'PK'
-		SET @chaine = @chaine +'#'
-	IF @var = 'F'
-		SET @chaine = @chaine +'@'
-	IF @var = 'UQ'
-		SET @chaine = @chaine +'!'
-	IF @var = 'C'
-		SET @chaine = @chaine +'%'
+SELECT @var = is_nullable FROM INFORMATION_SCHEMA.COLUMNS 
+WHERE column_name = @attribut AND TABLE_NAME = @taable 
+IF @var = 'YES' 
+	SET @chaine = @chaine +'?'
+DECLARE  curcons cursor for SELECT ao.type 
+			    FROM sys.all_objects ao 
+			    INNER JOIN INFORMATION_SCHEMA.CONSTRAINT_COLUMN_USAGE ic 
+ 			    ON ic.CONSTRAINT_NAME = ao.name
+ 			    WHERE ic.column_name = @attribut AND ic.TABLE_NAME = @taable
+ 	OPEN curcons
+ 	FETCH next from curcons into @var2
+ 	WHILE @@fetch_status = 0
+ 	BEGIN
+	 	IF @var2 = 'PK'
+			SET @chaine = @chaine +'#'
+		IF @var2 = 'F'
+			SET @chaine = @chaine +'@'
+		IF @var2 = 'UQ'
+			SET @chaine = @chaine +'!'
+		IF @var2 = 'C'
+			SET @chaine = @chaine +'%'
+	FETCH next from curcons into @var2
+ 	END
+ 	CLOSE curcons
+ 	DEALLOCATE curcons
+close dync
 SET @chaine = @chaine + @attribut
+PRINT 'valeur de chaine '+@chaine
+
+OPEN dync
+FETCH NEXT FROM dync INTO @db, @schem, @taable, @attribut
 WHILE @@FETCH_STATUS = 0
 	BEGIN
 		SET @prec = @taable
@@ -85,6 +100,7 @@ WHILE @@FETCH_STATUS = 0
 	END
 CLOSE dync
 DEALLOCATE dync
+
 
 
 /* fiche d'identité

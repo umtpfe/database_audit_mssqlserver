9ad6225daf8bd8d5a48cff5ff8e74374dffa2394
UMTPFE <47662521+umtpfe@users.noreply.github.com>
2019-04-10 10:29:53 +0100

    amélioration de la règle 2002

--- idrule_02002.sql
+++ idrule_02002.sql
@@ -2,7 +2,7 @@
 
 DECLARE @sql nvarchar(max);
 SET @sql = N'SELECT CAST(''master'' AS sysname) AS db_name, name schema_name, schema_id, CAST(1 AS int) AS database_id FROM master.sys.schemas ';
-SELECT @sql = @sql + N' UNION ALL SELECT ' + quotename(name,'''')+ ', name schema_name, schema_id, ' + CAST(database_id AS nvarchar(10)) + N' FROM ' + quotename(name) + N'.sys.schemas'
+SELECT @sql = @sql + N' UNION ALL SELECT ' + quotename(name,'''')+ N', name schema_name, schema_id, ' + CAST(database_id AS nvarchar(10)) + N' FROM ' + quotename(name) + N'.sys.schemas'
 FROM sys.databases WHERE database_id > 1
 AND state = 0
 AND user_access = 0;

748e646c4911bcd20ae95d5ce3eb670029af61c9
UMTPFE <47662521+umtpfe@users.noreply.github.com>
2019-04-10 10:25:22 +0100

    script pour toutes les requêtes

--- requetes.sql
+++ requetes.sql
@@ -581,7 +581,7 @@ GO
 PRINT char(13)+'id_rule = 2002'+char(13)+'Titre = liste des bases et schemas par serveur'
 DECLARE @sql nvarchar(max);
 SET @sql = N'SELECT CAST(''master'' AS sysname) AS db_name, name schema_name, schema_id, CAST(1 AS int) AS database_id FROM master.sys.schemas ';
-SELECT @sql = @sql + N' UNION ALL SELECT ' + quotename(name,'''')+ ', name schema_name, schema_id, ' + CAST(database_id AS nvarchar(10)) + N' FROM ' + quotename(name) + N'.sys.schemas'
+SELECT @sql = @sql + N' UNION ALL SELECT ' + quotename(name,'''')+ N', name schema_name, schema_id, ' + CAST(database_id AS nvarchar(10)) + N' FROM ' + quotename(name) + N'.sys.schemas'
 FROM sys.databases WHERE database_id > 1
 AND state = 0
 AND user_access = 0;

b2212b0c6c67bc2f3e5757b1d42fd678208d8293
UMTPFE <47662521+umtpfe@users.noreply.github.com>
2019-04-11 16:46:07 +0100

    nouvelle règles de conception

---
+++ idrule_02004.sql
@@ -0,0 +1,36 @@
+/*tables ayant les memes colonnes et de memes type*/
+
+DECLARE cur CURSOR FOR SELECT name FROM sys.tables order by name
+DECLARE @model SYSNAME
+OPEN cur 
+FETCH NEXT FROM cur INTO @model
+WHILE @@FETCH_STATUS = 0
+BEGIN
+	PRINT 'tables de memes nom de colonnes et de colonnes de memes type que '+@model
+	SELECT t.name FROM sys.tables AS t
+	WHERE name <> @model
+	AND EXISTS
+	(
+ 	 SELECT 1 FROM sys.columns AS c 
+ 	   WHERE [object_id] = t.[object_id]
+  	  AND EXISTS
+  	  (
+   	   SELECT 1 FROM sys.columns
+   	   WHERE [object_id] = OBJECT_ID(N'dbo.'+quotename(@model))
+   	   and  TYPE_NAME(user_type_id) = TYPE_NAME(c.user_type_id) 
+   	   AND name = c.name
+  	  )
+	)
+	order by t.name
+	FETCH NEXT FROM cur INTO @model
+END
+CLOSE cur 
+DEALLOCATE cur 
+
+/* fiche d'identitĂ©
+role = DBA
+categorie = CON
+type = SQL
+basetype  = sqlserver
+url = https://docs.microsoft.com/fr-fr/sql/t-sql/functions/type-name-transact-sql?view=sql-server-2017
+etat = 1 */
---
+++ idrule_02005.sql
@@ -0,0 +1,37 @@
+/*tables ayant les memes colonnes et de memes type et de meme taille*/
+
+DECLARE cur CURSOR FOR SELECT name FROM sys.tables order by name
+DECLARE @model SYSNAME
+OPEN cur 
+FETCH NEXT FROM cur INTO @model
+WHILE @@FETCH_STATUS = 0
+BEGIN
+	PRINT 'tables de memes nom de colonnes, de colonnes de memes type et de meme taille que '+@model
+	SELECT t.name FROM sys.tables AS t
+	WHERE name <> @model
+	AND EXISTS
+	(
+ 	 SELECT 1 FROM sys.columns AS c 
+ 	   WHERE [object_id] = t.[object_id]
+  	  AND EXISTS
+  	  (
+   	   SELECT 1 FROM sys.columns
+   	   WHERE [object_id] = OBJECT_ID(N'dbo.'+quotename(@model))
+   	   and  TYPE_NAME(user_type_id) = TYPE_NAME(c.user_type_id) 
+   	   and max_length = col_length(t.name, c.name)
+   	   AND name = c.name
+  	  )
+	)
+	order by t.name
+	FETCH NEXT FROM cur INTO @model
+END
+CLOSE cur 
+DEALLOCATE cur 
+
+/* fiche d'identitĂ©
+role = DBA
+categorie = CON
+type = SQL
+basetype  = sqlserver
+url = https://docs.microsoft.com/fr-fr/sql/t-sql/functions/col-length-transact-sql?view=sql-server-2017
+etat = 1 */

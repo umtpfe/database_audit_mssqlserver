c80448308a0b6920bc06f8ac54f043589b0a40d9
UMTPFE <47662521+umtpfe@users.noreply.github.com>
2019-05-09 15:09:41 +0100

    Requêtes de categorie INF

---
+++ idrule_00815.sql
@@ -0,0 +1,35 @@
+/* Determination des 50 objets les plus larges */
+
+SELECT 
+    t.name AS TableName,
+    i.name as indexName,
+    sum(p.rows) as RowCounts,
+    sum(a.total_pages) as TotalPages, 
+    sum(a.used_pages) as UsedPages, 
+    sum(a.data_pages) as DataPages,
+    (sum(a.total_pages) * 8) / 1024 as TotalSpaceMB, 
+    (sum(a.used_pages) * 8) / 1024 as UsedSpaceMB, 
+    (sum(a.data_pages) * 8) / 1024 as DataSpaceMB
+FROM 
+    sys.tables t
+INNER JOIN      
+    sys.indexes i ON t.object_id = i.object_id
+INNER JOIN 
+    sys.partitions p ON i.object_id = p.object_id AND i.index_id = p.index_id
+INNER JOIN 
+    sys.allocation_units a ON p.partition_id = a.container_id
+WHERE 
+    t.name NOT LIKE 'dt%' AND
+    i.object_id > 255 AND  
+    i.index_id <= 1
+GROUP BY 
+    t.name, i.object_id, i.index_id, i.name 
+ORDER BY SUM(p.rows) DESC
+
+/* fiche d'identite
+role = DBA
+categorie = INF
+type = SQL
+basetype = sqlserver
+url = NULL
+etat = 1 */
---
+++ idrule_00926.sql
@@ -0,0 +1,11 @@
+/* Vérifier l'horaire de démarrage de la base */
+
+SELECT sqlserver_start_time FROM sys.dm_os_sys_info
+
+/* fiche d'identite
+role = DBA
+categorie = INF
+type = SQL
+basetype = sqlserver
+url = NULL
+etat = 1 */
